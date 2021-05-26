8e0dec08e1e7138c147f73535fcd0a9b5164faa6
AdrielGraminho <adrielgraminho@gmail.com>
2021-05-26 19:58:51 -0300

    adicionando swagger

--- back/src/main/java/com/ecomerce/controller/UserController.java
+++ back/src/main/java/com/ecomerce/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.ecomerce.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    @ApiOperation(value = "Return an string 'User'")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success method return")
+    })
     @GetMapping
-    public String hello() { return "Usuário";}
+    public String hello() { return "User Api";}
 }
