1e23d58cefb54b0cc07bf0c5eadb6006fa02078a
Adriel Graminho <adrielgraminho@gmail.com>
2021-07-08 15:08:54 -0300

    adicionando swagger

--- Backend-Product/src/main/java/com/product/controller/ProductController.java
+++ Backend-Product/src/main/java/com/product/controller/ProductController.java
@@ -2,53 +2,93 @@ package com.product.controller;
 
 import com.product.entities.Product;
 import com.product.repository.ProductRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin
 @RestController
 @AllArgsConstructor
 public class ProductController {
 
     ProductRepository repository;
 
+    @ApiOperation(value = "return all products")
     @GetMapping("/product")
-    public List<Product> getAllProducts(){
-        return repository.findAll();
-    }
-
-    @GetMapping("/product/{id}")
-    public Product getProductById(@PathVariable Long id){
-        return repository.findById(id).get();
-    }
-
-    @PostMapping("/product")
-    public Product saveProduct(@RequestBody Product product){
-        return repository.save(product);
-    }
-
-    @DeleteMapping("/product/{id}")
-    public void deleteProduct(@PathVariable Long id){
-        repository.deleteById(id);
-    }
-
-    @RequestMapping(value = "/product/{id}", method =  RequestMethod.PUT)
-    public Product updateProduct(@PathVariable(value = "id") long id, @RequestBody Product newProduct) {
-        Optional<Product> oldProduct = repository.findById(id);
-        if (oldProduct.isPresent()) {
-            Product product = oldProduct.get();
-            product.setId(id);
-            product.setName(newProduct.getName());
-            product.setPreco(newProduct.getPreco());
-            product.setDescription(newProduct.getDescription());
-            return repository.save(product);
+    public ResponseEntity getAllProducts(){
+        try {
+            return new ResponseEntity<>( repository.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>( HttpStatus.METHOD_NOT_ALLOWED);
         }
-        else
-            return newProduct;
+    }
+    @ApiOperation(value = "return product by id")
+    @ApiResponses(
+            @ApiResponse(code = 200, message = "")
+    )
+    @GetMapping("/product/{id}")
+    public ResponseEntity getProductById(@PathVariable Long id){
+        try {
+            return new ResponseEntity<>( repository.findById(id).get(), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>( "ProductNotFound", HttpStatus.METHOD_NOT_ALLOWED);
+        }
+    }
+
+    @ApiOperation(value = "save new product")
+    @ApiResponses(
+            @ApiResponse(code = 200, message = "success")
+    )
+    @PostMapping("/product")
+    public ResponseEntity saveProduct(@RequestBody Product product){
+        try {
+            repository.save(product);
+            return new ResponseEntity<>( "success", HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>( "error", HttpStatus.METHOD_NOT_ALLOWED);
+        }
+    }
+
+    @ApiOperation(value = "delete product by id")
+    @ApiResponses(
+            @ApiResponse(code = 200, message = "success")
+    )
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity deleteProduct(@PathVariable Long id){
+        try {
+            repository.deleteById(id);
+            return new ResponseEntity<>( "success", HttpStatus.OK);
+        }catch (Exception e ){
+            return new ResponseEntity<>( "error", HttpStatus.METHOD_NOT_ALLOWED);
+        }
+    }
+
+    @ApiOperation(value = "edit product by id")
+    @ApiResponses(
+            @ApiResponse(code = 200, message = "success")
+    )
+    @PutMapping(value = "/product/{id}")
+    public ResponseEntity updateProduct(@PathVariable(value = "id") long id, @RequestBody Product newProduct) {
+        Optional<Product> oldProduct = repository.findById(id);
+        try {
+            if (oldProduct.isPresent()) {
+                Product product = oldProduct.get();
+                product.setId(id);
+                product.setName(newProduct.getName());
+                product.setPreco(newProduct.getPreco());
+                product.setDescription(newProduct.getDescription());
+                repository.save(product);
+                return new ResponseEntity<>("success", HttpStatus.OK);
+            }else {
+                return new ResponseEntity<>("error", HttpStatus.METHOD_NOT_ALLOWED);
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>("error-validation", HttpStatus.METHOD_NOT_ALLOWED);
+        }
     }
 }
